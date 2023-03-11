28464197cf7501fbd9219dad9e1d4d1e75e8101b
sameermd16 <sameerthemc@gmail.com>
2023-03-11 22:28:51 +0530

    minor changes

--- crowdfunding/src/components/Thanks.js
+++ crowdfunding/src/components/Thanks.js
@@ -4,10 +4,10 @@ import Check from "../images/icon-check.svg"
 const Thanks = () => {
   return (
     <>
-     <section className='thanks_div p-5 bg-white mx-5 mb-5 shaodw-sm'>
+     <section className='thanks_div p-5 bg-white mx-5 mb-10 shaodw-lg rounded-lg'>
         <img src={Check} alt="check_icon"/>
         <p className='font-bold text-xl my-5'>Thanks for your support!</p>
-        <p className='text-center'>Your pledge brings us one step closer to sharing Mastercraft Bamboo Monitor Riser worldwide. You will get
+        <p className='text-center thanks_para'>Your pledge brings us one step closer to sharing Mastercraft Bamboo Monitor Riser worldwide. You will get
             an email once our campaign is completed.
         </p>
         <button>Got it!</button>
